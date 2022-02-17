182826c2f50b56138ad6bced44546278d0d5c55b
Peter Lewandowski <mapiotr2010@gmail.com>
2022-02-17 10:57:17 -0500

    framed out API

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'); // import express ES5 style
+const cors = require('cors'); // allows cross origin resource sharing
+const { getTasks } = require('./src/tasks');
+const PORT = process.env.PORT || 3000; // establish a port to be used, check if there is an environmental variable which has already set a port to be used
+
+const app = express(); // creates an express app
+app.use(cors()); // hopefully the two `cors` lines will do what we need
+app.use(express.json()); 
+
+// Routes
+// app.post('/tasks', createTask);
+app.get('/tasks', getTasks);
+// app.patch('/tasks/:taskId', updateTask); // because of the `:` it takes an actual task Id in the path
+
+
+app.listen(PORT,() => {
+    console.log('Listening on Port: ', PORT)
+});
---
+++ src/tasks.js
@@ -0,0 +1,13 @@
+
+
+// exports.createTask = (request, response) => {  // ES5 way
+
+// }
+
+exports.getTasks = (request, response) => {
+    response.send('Get Tasks is working');
+}
+
+// exports.updateTask = (request, response) => {
+
+// }
